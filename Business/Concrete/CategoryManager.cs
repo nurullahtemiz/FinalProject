a95a783e1089986a2339bd8446e38e415b89040f
nurullah temiz <nurullahtemiz@hotmail.com>
2021-03-16 20:22:44 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _ctegoryDal;
+
+        public CategoryManager(ICategoryDal ctegoryDal)
+        {
+            _ctegoryDal = ctegoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //iş kodları
+            return _ctegoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _ctegoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
